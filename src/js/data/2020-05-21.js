e4e170aa1478ed4a5c2055a5af39d6d6d0cc04a3
hohuri <52710503+hohuri@users.noreply.github.com>
2020-05-21 11:10:29 -0500

    Update 2020-05-21.js

--- src/js/data/2020-05-21.js
+++ src/js/data/2020-05-21.js
@@ -1,4 +1,4 @@
-dataSetVersion = "2020-01-20"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSetVersion = "2020-05-21"; // Change this when creating a new data set version. YYYY-MM-DD format.
 dataSet[dataSetVersion] = {};
 
 dataSet[dataSetVersion].options = [
